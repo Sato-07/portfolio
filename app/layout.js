06a1d208038c805ac7bdf5884ba593100d3adcff
Sato-07 <nivellesachraf@gmail.com>
2024-02-15 23:58:24 +0100

    feat

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>SmartEvr</title>
+        <title>EvrSmartLabs</title>
       </head>
       <body>
         {/* <body className={`${font.className} `}> */}
